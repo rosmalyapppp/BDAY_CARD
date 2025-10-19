ee87f28f242ac6d064ccf0c918b459e8780d4ab6
rosmalyapppp <rosmalyap@gmail.com>
2025-10-19 16:35:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -27,11 +27,7 @@ function toggleMusic() {
 
 function blowCandles() {
     const wishMessage = document.getElementById('wishMessage');
-    wishMessage.innerHTML = `
-        <img src="bubble.png" alt="Sparkle" class="inline w-6 h-6 mx-1 animate-pulse">
-        Your wish has been made! May all your dreams come true!
-        <img src="bubble.png" alt="Sparkle" class="inline w-6 h-6 mx-1 animate-pulse">
-    `;
+   wishMessage.textContent = '🌟 Your wish has been made! May all your dreams come true! 🌟';
     wishMessage.classList.remove('opacity-0');
     wishMessage.classList.add('opacity-100', 'text-pink-150');
 
@@ -117,6 +113,7 @@ function playWish(wishNumber) {
 document.addEventListener('DOMContentLoaded', function() {
     generateCalendar();
 });
+
 
 
 
